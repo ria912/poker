fe7cd5f5c0623a9160d644510a486391b2a64e49
ria912 <ria912sh@gmail.com>
2025-05-01 21:32:00 +0900

    position.py を更新
    gptのコード

--- models/position.py
+++ models/position.py
@@ -1,21 +1,30 @@
+# models/position.py
 
-assignment_order = ['BTN', 'SB', 'BB', 'LJ', 'HJ', 'CO']
+ASSIGNMENT_ORDER = ['BTN', 'SB', 'BB', 'LJ', 'HJ', 'CO']
+FULL_POSITIONS = ['BTN', 'SB', 'BB', 'CO', 'HJ', 'LJ']
 
-# プレイヤーを回転させる
 def rotate_players(players):
+    """プレイヤーを1つ後ろに回して、BTNを進める"""
     players.append(players.pop(0))
 
-# プレイヤーの位置を割り当てる
 def assign_positions(players):
-    # 使うポジションの順番
-    use_positions = ['BTN', 'SB', 'BB', 'CO', 'HJ', 'LJ']
+    """
+    アクティブプレイヤーにポジションを割り当てる。
+    - FULL_POSITIONS の順に必要数だけポジションを切り出す。
+    - その中から ASSIGNMENT_ORDER の順で割り当てる。
+    """
     active_players = [p for p in players if not p.has_left]
 
-    available = use_positions[:len(active_players)]
-    assigned = [p for p in assignment_order if p in available]
+    # アクティブプレイヤー数だけポジション候補を取り出す
+    available_positions = FULL_POSITIONS[:len(active_players)]
 
-    for player in players:
-        player.position = None
+    # assignment_order の優先順でポジションを並べ替える
+    ordered_positions = [pos for pos in ASSIGNMENT_ORDER if pos in available_positions]
 
-    for player, pos in zip(active_players, assigned):
+    # 全プレイヤーのポジション初期化
+    for p in players:
+        p.position = None
+
+    # アクティブプレイヤーに順番にポジションを割り当てる
+    for player, pos in zip(active_players, ordered_positions):
         player.position = pos
