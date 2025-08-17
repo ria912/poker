49c23021ead0e888a6076e75d1154c516a6591cd
ria912 <ria912sh@gmail.com>
2025-08-17 12:07:28 +0900

    game_service.py を更新

--- app/services/game_service.py
+++ app/services/game_service.py
@@ -1,26 +1,46 @@
-from ..models.game_state import GameState
-from ..models.enum import Round
-from . import table_service, deck_service
+    # --- ゲーム進行制御メソッド ---
 
-def start_new_hand(game_state: GameState):
-    """新しいハンドを開始"""
-    table_service.reset_table(game_state)
-    deck_service.reset_deck(game_state)
-    # 各プレイヤーに2枚配る
-    for i, seat in enumerate(game_state.table.seats):
-        if seat.is_occupied:
-            deck_service.deal_to_player(game_state)
+    def start_hand(self):
+        """新しいハンドを開始"""
+        self.table.reset_for_new_hand()
+        self.round = Round.PREFLOP
+        self.state = State.RUNNING
+        self.set_dealer_and_blinds()
 
-def next_round(game_state: GameState):
-    """ラウンド進行"""
-    if game_state.table.current_round == Round.PREFLOP:
-        deck_service.deal_to_table(game_state, 3)  # Flop
-        game_state.table.current_round = Round.FLOP
-    elif game_state.table.current_round == Round.FLOP:
-        deck_service.deal_to_table(game_state, 1)  # Turn
-        game_state.table.current_round = Round.TURN
-    elif game_state.table.current_round == Round.TURN:
-        deck_service.deal_to_table(game_state, 1)  # River
-        game_state.table.current_round = Round.RIVER
-    elif game_state.table.current_round == Round.RIVER:
-        game_state.table.current_round = Round.SHOWDOWN
+    def set_dealer_and_blinds(self):
+        """ディーラーとブラインドを設定"""
+        self.dealer_index = self.table.get_next_occupied_seat(self.dealer_index)
+        sb_index = self.table.get_next_occupied_seat(self.dealer_index)
+        bb_index = self.table.get_next_occupied_seat(sb_index)
+
+        self.table.seats[sb_index].place_bet(self.small_blind)
+        self.table.seats[bb_index].place_bet(self.big_blind)
+
+        self.current_player_index = self.table.get_next_occupied_seat(bb_index)
+
+    def proceed_to_next_round(self):
+        """次のラウンドに進む"""
+        if self.round == Round.PREFLOP:
+            self.round = Round.FLOP
+            self.table.deal_flop()
+        elif self.round == Round.FLOP:
+            self.round = Round.TURN
+            self.table.deal_turn()
+        elif self.round == Round.TURN:
+            self.round = Round.RIVER
+            self.table.deal_river()
+        elif self.round == Round.RIVER:
+            self.round = Round.SHOWDOWN
+            self.state = State.SHOWDOWN
+        else:
+            self.state = State.FINISHED
+
+    def get_current_player(self):
+        """現在アクションすべきプレイヤーを返す"""
+        if self.current_player_index is None:
+            return None
+        return self.table.seats[self.current_player_index].player
+
+    def advance_turn(self):
+        """次のプレイヤーにターンを回す"""
+        self.current_player_index = self.table.get_next_occupied_seat(self.current_player_index)
