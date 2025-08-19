8aebf21ae152d9dc55b7f5e3628b29f2d1fe24fe
ria912 <ria912sh@gmail.com>
2025-08-19 12:40:25 +0900

    game_state.py を更新

--- app/models/game_state.py
+++ app/models/game_state.py
@@ -17,4 +17,13 @@ class GameState(BaseModel):
     dealer_index: Optional[int] = None
     small_blind: int = 50
     big_blind: int = 100
+
+    @property
+    def players(self) -> List[Player]:
+        return [s.player for s in self.table.seats if s.player is not None]
+
+    @property
+    def active_seats(self) -> List[int]:
+        return [s.index for s in self.table.seats
+                if s.player and s.player.state not in (PlayerState.FOLDED, PlayerState.OUT, PlayerState.SITTING_OUT)]
     
