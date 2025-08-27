fc5e2c7f844dee853369053b6e6552d5b9633308
ria912 <ria912sh@gmail.com>
2025-08-27 12:48:19 +0900

    deck.py を更新

--- app/models/deck.py
+++ app/models/deck.py
@@ -2,7 +2,7 @@ import random
 from typing import List, Tuple
 
 class Deck:
-    suits = ["♠", "♥", "♦", "♣"]
+    suits = ["s", "h", "d", "c"]
     ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"]
 
     def __init__(self):
